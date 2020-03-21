bb7b16490b8883537b54f532df646a83ae05a05e
minhquan3197 <minhquan.k0r1@gmail.com>
2020-03-21 12:04:45 +0700

    Basic swagger

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+var helmet_1 = __importDefault(require("helmet"));
+var routes_1 = require("./routes/routes");
 var constants_1 = require("./config/constants");
 var app = express_1.default();
-app.use(express_1.default.json());
+app.use(helmet_1.default());
+app.use(cors_1.default());
+app.use(body_parser_1.default.json());
+routes_1.RegisterRoutes(app);
+try {
+    var swaggerDocument = require('../dist/swagger.json');
+    app.use('/docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
+}
+catch (err) {
+    console.log('Unable to load swagger.json', err);
+}
 app.listen(constants_1.PORT, function () {
     console.log("Server is listening on port " + constants_1.PORT);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import helmet from 'helmet';
+import { RegisterRoutes } from './routes/routes';
 import { PORT } from './config/constants';
 
 const app = express();
 
-app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+
+RegisterRoutes(app);
+
+try {
+    const swaggerDocument = require('../dist/swagger.json');
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+} catch (err) {
+    console.log('Unable to load swagger.json', err);
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
