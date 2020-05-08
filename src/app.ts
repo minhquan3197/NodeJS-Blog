1cc31a04a8cf61021e858413224b0d5c9aef9e0b
minhquan3197 <minhquan.k0r1@gmail.com>
2020-05-08 07:29:35 +0700

    cluster

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import cors from 'cors';
+import morgan from 'morgan';
 import helmet from 'helmet';
 import express from 'express';
 import passport from 'passport';
@@ -21,6 +22,7 @@ class App {
         this.app.use(passport.initialize());
         this.app.use(cors());
         this.app.use(helmet());
+        this.app.use(morgan('combined'));
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(bodyParser.json());
     };
